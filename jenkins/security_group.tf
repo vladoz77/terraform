b540559b2f094fd6e327132183ec487e0e71311e
Kovalev Vladislav <vladoz77@yandex.ru>
2025-04-04 16:59:33 +0300

    add ansible provision jenkins and jenkins agent

--- jenkins/security_group.tf
+++ jenkins/security_group.tf
@@ -7,22 +7,43 @@ resource "yandex_vpc_security_group" "vpc_group" {
     protocol = "TCP"
     port = 22
     v4_cidr_blocks = ["0.0.0.0/0"]
+    description = "accept all trafic to 22 port"
   }
 
   ingress {
     protocol = "TCP"
     port = 8080
     v4_cidr_blocks = ["0.0.0.0/0"]
+    description = "accept all trafic to 8080 port"
+
   }
 
   ingress {
     protocol = "TCP"
     port = 80
     v4_cidr_blocks = ["0.0.0.0/0"]
+    description = "accept all trafic to 8080 port"
+  }
+
+   ingress {
+    protocol = "ANY"
+    from_port = 0
+    to_port = 65535
+    v4_cidr_blocks = ["172.16.10.0/24"]
+    description = "accept all ingress trafic from cidr 172.16.10.0/24"
   }
 
   egress {
     protocol = "ANY"
     v4_cidr_blocks = ["0.0.0.0/0"]
+    description = "accept any egress trafic from any address"
+  }
+
+  egress {
+    protocol = "ANY"
+    from_port = 0
+    to_port = 65535
+    v4_cidr_blocks = ["172.16.10.0/24"]
+    description = "accept egress trafic from CIDR 172.16.10.0/24"
   }
 }
--- jenkins/terraform.tfvars
+++ jenkins/terraform.tfvars
@@ -1,2 +1,2 @@
 role_id = "fac78a2a-ed15-7dd1-8a23-dcab95b63db8"
-secret_id = "bc6189ad-eb55-d1a5-3ad1-53823f35c509"
+secret_id = "ef4df51b-8718-85ab-0eda-f5911644c630"
