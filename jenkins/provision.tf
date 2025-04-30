825feb004ab56b778c954db2a6f624c64638dafc
Kovalev Vladislav <vladoz77@yandex.ru>
2025-04-30 15:57:19 +0300

    fix sonarqube

--- jenkins/provision.tf
+++ jenkins/provision.tf
@@ -1,16 +1,25 @@
+# Ресурс для генерации динамического inventory-файла Ansible
 resource "local_file" "inventory" {
+  # Используем шаблон для генерации содержимого файла
   content = templatefile("./provision/inventory.tftpl",
     {
+      # Передаем в шаблон публичные IP-адреса Jenkins-серверов
       jenkins       = flatten(module.jenkins[*].public_ips)
+      # Передаем публичные IP-адреса агентов Jenkins
       jenkins-agent = flatten(module.jenkins-agent[*].public_ips)
+      # Имя пользователя для подключения
       username   = var.username
     }
   )
+  # Путь для сохранения inventory-файла
   filename   = "./provision/inventory.ini"
+  # Ожидаем создания Jenkins и агентов перед генерацией файла
   depends_on = [module.jenkins, module.jenkins-agent]
 }
 
+# Ресурс для выполнения Ansible-плейбуков
 resource "null_resource" "run-ansible" {
+  # Триггеры для повторного запуска при изменении ID инстансов
   triggers = {
     id = join(",", flatten([
       module.jenkins[*].instance_id,
@@ -18,9 +27,12 @@ resource "null_resource" "run-ansible" {
       ]))
   }
 
+  # Проверка доступности SSH на всех хостах перед выполнением плейбуков
   provisioner "local-exec" {
     command = <<EOT
+      # Извлекаем все IP-адреса из inventory-файла
       for ip in $(cat provision/inventory.ini | grep -E '^[0-9]' | awk '{print $1}'); do
+        # Ожидаем пока порт 22 (SSH) не станет доступен
         until nc -zv $ip 22; do
           echo "Waiting for SSH on $ip..."
           sleep 5
@@ -29,10 +41,15 @@ resource "null_resource" "run-ansible" {
     EOT
   }
 
-  # Запустим плейбук
+  # Выполняем плейбук
   provisioner "local-exec" {
-    command = "ansible-playbook -i provision/inventory.ini provision/provision.yaml -u ${var.username}"
+    command = "ansible-playbook -i provision/inventory.ini provision/provision-all.yaml -u ${var.username}"
   }
 
-  depends_on = [module.jenkins, module.jenkins-agent, local_file.inventory]
+  # Зависимости - ждем создания всех ресурсов перед выполнением
+  depends_on = [
+    module.jenkins,         # Модуль Jenkins-сервера
+    module.jenkins-agent,   # Модуль Jenkins-агентов
+    local_file.inventory    # Сгенерированный inventory-файл
+  ]
 }
--- jenkins/terraform.tfvars
+++ jenkins/terraform.tfvars
@@ -1,2 +1,2 @@
 role_id = "fac78a2a-ed15-7dd1-8a23-dcab95b63db8"
-secret_id = "ef4df51b-8718-85ab-0eda-f5911644c630"
+secret_id = "b52e5c77-347d-e7b0-40b9-3b419b0c0b00"
