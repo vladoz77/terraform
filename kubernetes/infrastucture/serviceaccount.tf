0773b3bb5df7e11032db08f930e09cd5bb4f692c
Kovalev Vladislav <vladoz77@yandex.ru>
2025-07-16 14:00:10 +0300

    fix sa-k8s-admin-permissions

--- kubernetes/infrastucture/serviceaccount.tf
+++ kubernetes/infrastucture/serviceaccount.tf
@@ -6,7 +6,7 @@ resource "yandex_iam_service_account" "sa-k8s-admin" {
 
 resource "yandex_resourcemanager_folder_iam_member" "sa-k8s-admin-permissions" {
   for_each = var.roles
-  folder_id = data.vault_kv_secret_v2.yc_creds.data["folder_id"]
+  folder_id = var.folder_id
   role      = each.value
   member    = "serviceAccount:${yandex_iam_service_account.sa-k8s-admin.id}"
 }
