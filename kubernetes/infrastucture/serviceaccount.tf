c74b26df7771d17eee2f6e941f4b28971d108cf4
Kovalev Vladislav <vladoz77@yandex.ru>
2025-07-17 14:12:21 +0300

    fix get-token.sh

--- kubernetes/infrastucture/serviceaccount.tf
+++ kubernetes/infrastucture/serviceaccount.tf
@@ -11,21 +11,21 @@ resource "yandex_resourcemanager_folder_iam_member" "sa-k8s-admin-permissions" {
   member    = "serviceAccount:${yandex_iam_service_account.sa-k8s-admin.id}"
 }
 
-resource "yandex_iam_service_account_key" "sa-k8s-admin-key" {
-  service_account_id = yandex_iam_service_account.sa-k8s-admin.id
-  description = "sa-k8s-admin-key file"
-  key_algorithm      = "RSA_2048"
-}
+# resource "yandex_iam_service_account_key" "sa-k8s-admin-key" {
+#   service_account_id = yandex_iam_service_account.sa-k8s-admin.id
+#   description = "sa-k8s-admin-key file"
+#   key_algorithm      = "RSA_2048"
+# }
 
-data "template_file" "sa_key_json" {
-  template = file("${path.module}/templates/sa-key.json.tpl")
+# data "template_file" "sa_key_json" {
+#   template = file("${path.module}/templates/sa-key.json.tpl")
 
-  vars = {
-    key_id      = yandex_iam_service_account_key.sa-k8s-admin-key.id
-    sa_id       = yandex_iam_service_account.sa-k8s-admin.id
-    private_key = yandex_iam_service_account_key.sa-k8s-admin-key.private_key
-  }
-}
+#   vars = {
+#     key_id      = yandex_iam_service_account_key.sa-k8s-admin-key.id
+#     sa_id       = yandex_iam_service_account.sa-k8s-admin.id
+#     private_key = yandex_iam_service_account_key.sa-k8s-admin-key.private_key
+#   }
+# }
 
 
 
