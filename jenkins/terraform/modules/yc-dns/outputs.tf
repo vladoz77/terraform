88e44511c5d2ddcb09174d05ce3d364bdee9720b
Kovalev Vladislav <vladoz77@yandex.ru>
2025-09-18 08:29:13 +0300

    rename old

--- jenkins/terraform/modules/yc-dns/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "zone_id" {
-  description = "DNS zone ID"
-  value       = var.create_zone ? yandex_dns_zone.zone[0].id : null
-}
-
-output "zone_name" {
-  description = "DNS zone name"
-  value       = var.create_zone ? yandex_dns_zone.zone[0].name : null
-}
