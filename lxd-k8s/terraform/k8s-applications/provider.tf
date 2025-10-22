bdbac7b0f6eda594f77127ec898daef32cc566c0
Kovalev Vladislav <vladoz77@yandex.ru>
2025-10-22 13:28:35 +0300

    add remote state for applications

--- lxd-k8s/terraform/k8s-applications/provider.tf
+++ lxd-k8s/terraform/k8s-applications/provider.tf
@@ -11,6 +11,20 @@ terraform {
     }
   }
 
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "vladis-terraform-state"
+    region = "ru-central1"
+    key    = "lxd/k8s-applications.tfstate" 
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+
 }
 
 provider "helm" {
